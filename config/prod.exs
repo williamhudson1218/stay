718e081c05634903c50b804572156efebd074a3b
William Hudson <williamhudson@Williams-MacBook-Pro.local>
2023-11-13 00:35:55 +0000

    update elixir.yml

--- config/prod.exs
+++ config/prod.exs
@@ -9,11 +9,13 @@ import Config
 config :stay, Stay.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "0360c2cc-3f6e-4bdf-ae42-b7f996a0bc75-user",
+  password: "pw-6bd8645d-7164-4072-b9ea-977fa3f33764",
+  database: "0360c2cc-3f6e-4bdf-ae42-b7f996a0bc75",
+  hostname: "postgres-free-tier-v2020.gigalixir.com",
+  port: 5432,
   url:
     {:system,
-     "postgresql://0360c2cc-3f6e-4bdf-ae42-b7f996a0bc75-user:pw-6bd8645d-7164-4072-b9ea-977fa3f33764@postgres-free-tier-v2020.gigalixir.com:5432/0360c2cc-3f6e-4bdf-ae42-b7f996a0bc75"},
-  ssl: true,
-  pool_size: 2
+     "postgresql://0360c2cc-3f6e-4bdf-ae42-b7f996a0bc75-user:pw-6bd8645d-7164-4072-b9ea-977fa3f33764@postgres-free-tier-v2020.gigalixir.com:5432/0360c2cc-3f6e-4bdf-ae42-b7f996a0bc75"}
 
 config :stay, StayWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
